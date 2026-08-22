f75c5e0e74f61d96b1cbb766bfd146bc34c83ecd
Aidan Tang <aidanroom124@gmail.com>
2026-08-22 15:56:39 -0400

    First commit. Contains backend framework to begin with.

---
+++ testmaker-backend/src/main/java/aidantang/testmaker_backend/TestmakerBackendApplication.java
@@ -0,0 +1,13 @@
+package aidantang.testmaker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmakerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmakerBackendApplication.class, args);
+	}
+
+}
---
+++ testmaker-backend/src/test/java/aidantang/testmaker_backend/TestmakerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package aidantang.testmaker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmakerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
